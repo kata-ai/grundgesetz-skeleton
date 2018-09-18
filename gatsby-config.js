1cd206d7c452aba1017645352869c0b98851b352
Resi Respati <resir014@gmail.com>
2018-09-18 18:06:41 +0700

    upgraded dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const cssnext = require('postcss-cssnext');
-
 module.exports = {
   siteMetadata: {
     title: 'Grundgesetz',
@@ -58,7 +56,6 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-resolve-src',
     'gatsby-plugin-typescript',
-    'gatsby-plugin-react-next',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet'
