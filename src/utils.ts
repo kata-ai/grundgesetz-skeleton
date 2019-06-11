dae80ceb7f87db13600ab3c48b0471e1dfa6ee30
Resi Respati <resir014@gmail.com>
2019-06-11 12:19:40 +0700

    fix: utils bug

---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export * from './utils/primitives';
+export * from './utils/variables';
+export * from './utils/renderAst';
--- src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './primitives';
-export * from './variables';
-export * from './renderAst';
