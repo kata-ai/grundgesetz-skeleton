bedd8c183d12aaacf1331122164bc14a4d936d63
Resi Respati <resir014@gmail.com>
2019-10-30 15:26:45 +0700

    fixed typography primitive

--- src/components/foundations/typography/components/Typography.tsx
+++ src/components/foundations/typography/components/Typography.tsx
@@ -1,39 +1,7 @@
 import styled from 'styled-components';
-import {
-  display,
-  maxWidth,
-  space,
-  fontFamily,
-  fontSize,
-  fontWeight,
-  lineHeight,
-  color,
-  textAlign,
-  verticalAlign,
-  letterSpacing,
-  FontFamilyProps,
-  FontSizeProps,
-  FontWeightProps,
-  LineHeightProps,
-  MaxWidthProps,
-  SpaceProps,
-  DisplayProps,
-  TextAlignProps,
-  VerticalAlignProps,
-  LetterSpacingProps
-} from 'styled-system';
+import { layout, LayoutProps, space, SpaceProps, color, ColorProps, typography, TypographyProps } from 'styled-system';
 
-export interface TypographyProps
-  extends DisplayProps,
-    MaxWidthProps,
-    SpaceProps,
-    FontFamilyProps,
-    FontSizeProps,
-    FontWeightProps,
-    LineHeightProps,
-    TextAlignProps,
-    VerticalAlignProps,
-    LetterSpacingProps {
+export interface TypographyProps extends LayoutProps, SpaceProps, ColorProps, TypographyProps {
   /** Extended color props. */
   color?: string;
 }
@@ -42,17 +10,10 @@ export interface TypographyProps
  * This is a base `Text` element to handle typography elements.
  */
 export const Typography = styled('span')<TypographyProps>`
-  ${display};
-  ${maxWidth};
-  ${space};
-  ${fontFamily}
-  ${fontSize};
-  ${fontWeight}
-  ${lineHeight};
-  ${color};
-  ${textAlign};
-  ${verticalAlign};
-  ${letterSpacing};
+  ${layout}
+  ${space}
+  ${color}
+  ${typography}
 `;
 
 Typography.displayName = 'Typography';
