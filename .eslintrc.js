284ed1752b07996ec37247d8274ea69c2690010f
Resi Respati <resir014@gmail.com>
2020-06-08 12:42:59 +0700

    more lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: ['kata/react', 'prettier', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
